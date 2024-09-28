c78b053ccc476e81ea2bafae0465011eaf97dbe0
Alex <puskin1914@gmail.com>
2024-09-28 19:31:36 +0000

    update tailwind config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 // import localFont from "next/font/local";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
+import { cn } from "@/shared/ui/utils";
 
 // const geistSans = localFont({
 //   src: "./fonts/GeistVF.woff",
