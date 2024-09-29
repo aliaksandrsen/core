3c2712410a061f8058af501871b035b1d5d58ba3
Alex <puskin1914@gmail.com>
2024-09-29 11:14:56 +0000

    add db init migration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import { Button } from "@/shared/ui/button";
+import { PrismaClient } from "@prisma/client";
 
-export default function Home() {
+const client = new PrismaClient();
+
+export default async function Home() {
+  const courses = await client.course.findMany();
+
+  console.log(courses);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button>Button</Button>
-      <p>12434</p>
     </main>
   );
 }
